e80ba03e80df448825b42e42f6edf9a898b29cec
Carlos <carlosepp@gmail.com>
2013-10-24 14:26:59 -0500

    window load

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -121,14 +121,16 @@ var table = $('#main-content table'),
 
       // Dialog sizes
 
+      $(window).load(function(e){
+        var width = $('.ms-dialog #main-content').width();
+        width = width + 'px !important';
+        console.log(width);
+        $('.ms-dlgFrameContainer, .ms-dlgFrameContainer iframe, .ms-dlgTitle, .ms-dlgFrame, .ms-dlgBorder').animate({
+          width: width
+        },200); 
+      });
 
-
-      var width = $('.ms-dialog #main-content').width();
-      width = width + 'px !important';
-      console.log(width);
-      $('.ms-dlgFrameContainer, .ms-dlgFrameContainer iframe, .ms-dlgTitle, .ms-dlgFrame, .ms-dlgBorder').css({
-        width: width
-      }); 
+      
       // Detect IE
 
       // IE feature detection
