226d1667e06e09948bc39edee817527d678cda04
Carlos <carlosepp@gmail.com>
2013-10-24 12:54:16 -0500

    dialogs

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -126,7 +126,7 @@ var table = $('#main-content table'),
       var width = $('.ms-dialog #main-content').width();
       width = width + 'px !important';
       console.log(width);
-      $('.ms-dlgFrameContainer').css({
+      $('.ms-dlgFrameContainer, .ms-dlgFrameContainer iframe, .ms-dlgTitle').css({
         width: width
       }); 
       // Detect IE
