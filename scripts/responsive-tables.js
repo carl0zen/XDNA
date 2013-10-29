16f8f493cb7bb38eb5626c2d2de9daf390cbf9eb
Carlos <carlosepp@gmail.com>
2013-10-29 11:01:04 -0600

    test

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -11,51 +11,6 @@ window.onload = init;
  
 
 $(document).ready(function(e){
-
-/**var head= $('.ms-viewheadertr'),
-    headText = head.html();
-    //**Takking the headers out as thead elements
-    head.parent().parent().prepend('<thead>'+headText+'</thead>');
-    head.remove();
-
-  var stackedTable = $('table#onetidMngFieldRptrTable, table#onetidMngCytpeRptrTable'),
-      headings = new Array(),
-      headingCount = 0;
-      
-      stackedTable.find('tbody tr th').each(function(e){
-      headings[headingCount] = $(this).text();
-      // stackedTable.find('tr').not('first-child').each(function(e){
-        
-        var   countIterator = 0;
-        $(this).find('tr td.ms-vb2').each(function(e){
-            var fieldValue = $(this).html();
-            $(this).html('<span class="field-name">'+ headings[countIterator] + '</span> ' + fieldValue);
-            countIterator++;
-            console.log('countIterator: ' + countIterator + 'headings[countIterator]: ' + headings[countIterator]);
-        });
-      //});
-      headingCount++;
-    }).parent().remove();
-
-  
-
-
-
-
-  var $sptableList = $("table.ms-listviewtable > tbody");
-  var $headers = $sptableList.find("> tr.ms-viewheadertr > th");
-  var $items = $sptableList.find("> tr.ms-itmhover");
-  $items.each(function() {
-    $item = $(this);
-    var counter = 0;
-    $cols = $item.find(" > td");
-    $cols.each(function() {
-      $mobileHeader = $("<div class='mob-header'></div>");
-      $mobileHeader.append($($headers[counter]).html());
-      $(this).prepend($mobileHeader);
-      counter++;
-    });
-  });**/
 var table = $('#main-content table'),
     heading = {
         number: null,
@@ -120,7 +75,16 @@ var table = $('#main-content table'),
       $(window).resize(function(e){ resetTable(); });
 
 
-      // Dialog sizes
+      
+ 
+   if($("div").attr("id") != "gridBoard") {
+    $("body").removeClass("internal");
+  } else {
+    
+  }
+
+/**
+    // Dialog sizes
 
 
 
@@ -129,14 +93,54 @@ var table = $('#main-content table'),
       $('.ms-dlgContent, .ms-dlgBorder, .ms-dlgTitle, .ms-dlgFrame, .ms-dlgFrameContainer').css({
         width: width + '!important',
       }); 
-      // Detect IE
 
-      // IE feature detection
-   if($("div").attr("id") != "gridBoard") {
-    $("body").removeClass("internal");
-  } else {
-    
-  }
+
+  
+  var head= $('.ms-viewheadertr'),
+    headText = head.html();
+    //**Takking the headers out as thead elements
+    head.parent().parent().prepend('<thead>'+headText+'</thead>');
+    head.remove();
+
+  var stackedTable = $('table#onetidMngFieldRptrTable, table#onetidMngCytpeRptrTable'),
+      headings = new Array(),
+      headingCount = 0;
+      
+      stackedTable.find('tbody tr th').each(function(e){
+      headings[headingCount] = $(this).text();
+      // stackedTable.find('tr').not('first-child').each(function(e){
+        
+        var   countIterator = 0;
+        $(this).find('tr td.ms-vb2').each(function(e){
+            var fieldValue = $(this).html();
+            $(this).html('<span class="field-name">'+ headings[countIterator] + '</span> ' + fieldValue);
+            countIterator++;
+            console.log('countIterator: ' + countIterator + 'headings[countIterator]: ' + headings[countIterator]);
+        });
+      //});
+      headingCount++;
+    }).parent().remove();
+
+  
+
+
+
+
+  var $sptableList = $("table.ms-listviewtable > tbody");
+  var $headers = $sptableList.find("> tr.ms-viewheadertr > th");
+  var $items = $sptableList.find("> tr.ms-itmhover");
+  $items.each(function() {
+    $item = $(this);
+    var counter = 0;
+    $cols = $item.find(" > td");
+    $cols.each(function() {
+      $mobileHeader = $("<div class='mob-header'></div>");
+      $mobileHeader.append($($headers[counter]).html());
+      $(this).prepend($mobileHeader);
+      counter++;
+    });
+  });**/
+
 
 });
 
