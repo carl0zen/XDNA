025d79cb6ce592a650b124eda138ee6a044701c8
Oscar Daniel Castañeda Rodriguez <oscar.d.rodriguez@accenture.com>
2013-10-30 13:55:52 -0600

    Commit

---
+++ scripts/responsive-tables.js
@@ -0,0 +1,148 @@
+function init() {
+if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)){ //test for MSIE x.x;
+     var ieversion=new Number(RegExp.$1);// capture x.x portion and store as a number
+     if (ieversion<=8){
+        $('html').addClass('lt-ie8');
+     }
+     
+    }
+}
+window.onload = init;
+ 
+
+$(document).ready(function(e){
+  if( $('#s4-leftpanel').height() > $('#main-content').height())
+    {
+      var height69 = $('#s4-leftpanel').height()
+      $('html:not(.ms-dialog) #main-content').height(height69)
+    }
+
+var table = $('#main-content table'),
+    heading = {
+        number: null,
+        value: null
+    },
+    counter = 1;
+    var headings= new Array();
+    
+    $('#main-content tbody th').each(function(e){
+        var value = $(this).text();
+        
+        heading.number = counter;
+        heading.value = value;
+        
+        headings[counter] = heading;
+        
+        table.find('tr > td.ms-vb2:nth-child('+heading.number+')').prepend('<span class="fieldName">'+heading.value+'</span>');
+        
+        counter++;
+    });
+
+    // Stacked table approach
+
+    var tableTitle = $('.ms-vb-title');
+
+    tableTitle.each(function(e){
+        var titleHtml = $(this).text();
+        titleHtml = $.trim(titleHtml);
+      
+        var thisParent = $(this).parent();
+        thisParent.before('<tr class="accordion-title"><td><div class="header-table">'+titleHtml+'</div></td></tr>');
+        //thisParent.hide();
+        
+
+    });
+
+    $('.accordion-title')
+          .on({
+            click: function(){
+
+              $('.accordion-title').not(this).removeClass('active').next().slideUp(100);
+
+
+
+              $(this).toggleClass("active").next().slideToggle(200);
+              
+            }
+          });
+
+      var resetTable = function(e){
+
+        var windowSize = $(window).width();
+        if (windowSize > 768) {
+          //Little fix to avoid display block while showing the element
+           $('.ms-itmhover').show(100,function(e){$(this).removeAttr('style');});
+               
+        }else{
+          $('.accordion-title').next().slideUp(100);
+        }
+      }
+      resetTable();
+      $(window).resize(function(e){ resetTable(); });
+
+
+      
+ 
+  if ($('#gridBoard')){$('body').removeClass('internal')}
+
+/**
+    // Dialog sizes
+
+
+
+      var width = $('.ms-dialog #main-content').width();
+
+      $('.ms-dlgContent, .ms-dlgBorder, .ms-dlgTitle, .ms-dlgFrame, .ms-dlgFrameContainer').css({
+        width: width + '!important',
+      }); 
+
+
+  
+  var head= $('.ms-viewheadertr'),
+    headText = head.html();
+    //**Takking the headers out as thead elements
+    head.parent().parent().prepend('<thead>'+headText+'</thead>');
+    head.remove();
+
+  var stackedTable = $('table#onetidMngFieldRptrTable, table#onetidMngCytpeRptrTable'),
+      headings = new Array(),
+      headingCount = 0;
+      
+      stackedTable.find('tbody tr th').each(function(e){
+      headings[headingCount] = $(this).text();
+      // stackedTable.find('tr').not('first-child').each(function(e){
+        
+        var   countIterator = 0;
+        $(this).find('tr td.ms-vb2').each(function(e){
+            var fieldValue = $(this).html();
+            $(this).html('<span class="field-name">'+ headings[countIterator] + '</span> ' + fieldValue);
+            countIterator++;
+            console.log('countIterator: ' + countIterator + 'headings[countIterator]: ' + headings[countIterator]);
+        });
+      //});
+      headingCount++;
+    }).parent().remove();
+
+  
+
+
+
+
+  var $sptableList = $("table.ms-listviewtable > tbody");
+  var $headers = $sptableList.find("> tr.ms-viewheadertr > th");
+  var $items = $sptableList.find("> tr.ms-itmhover");
+  $items.each(function() {
+    $item = $(this);
+    var counter = 0;
+    $cols = $item.find(" > td");
+    $cols.each(function() {
+      $mobileHeader = $("<div class='mob-header'></div>");
+      $mobileHeader.append($($headers[counter]).html());
+      $(this).prepend($mobileHeader);
+      counter++;
+    });
+  });**/
+
+
+});
+
